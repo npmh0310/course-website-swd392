68794ff12d12a2397d292e9e601bc1dfcaa6effe
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-03-06 11:01:17 +0700

    home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line no-unused-vars
+import { React } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import PathPage from './pages/PathPage';
+import SignUpPage from './pages/SignUpPage';
+import BlogPage from "./pages/BlogPage";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <BrowserRouter >
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="signup" element={<SignUpPage />} />
+      <Route path="path" element={<PathPage />} />
+      <Route path="blog" element={<BlogPage />} />
+    </Routes>
+  </BrowserRouter>
+
 }
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Logo from '../../assets/svgexport-1.svg'
+import { footer } from "../../data/data";
+import { HiOutlineChevronRight } from "react-icons/hi";
+import './footer.css'
+const Footer = () => {
+  const { company, social, support } = footer
+  return <footer className='bg-primary  section'>
+    <div className='container mx-auto'>
+      <div className="flex flex-col gap-y-8 lg:flex-row lg:gap-x-20  ">
+        <div className="mb-5">
+          <img src={Logo} className="w-28 lg:w-36" alt="" />
+        </div>
+        <div className="flex flex-row w-full lg:w-[50%]" >
+          <div className="flex-grow">
+            <div><h1 className="title lg:text-2xl">{company.name}</h1></div>
+            <div className="flex flex-col gap-y-3">
+              <ul className="space-y-3">
+                {company.items.map((item, index) => {
+                  return <li className="text-md" key={index}>
+                    <a className="footer-link" key={index} href={item.link}>{item.title}</a>
+                  </li>
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="flex-grow">
+            <div><h1 className="title lg:text-2xl">{social.name}</h1></div>
+            <div className="flex flex-col gap-y-3">
+              <ul className="space-y-3">
+                {social.items.map((item, index) => {
+                  return <li className="text-md" key={index}>
+                    <a className="footer-link" key={index} href={item.link}>{item.title}</a>
+                  </li>
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="flex-grow">
+            <div><h1 className="title lg:text-2xl">{support.name}</h1></div>
+            <div className="flex flex-col gap-y-3">
+              <ul className="space-y-3">
+                {support.items.map((item, index) => {
+                  return <li className="text-md" key={index}>
+                    <a className="footer-link" key={index} href={item.link}>{item.title}</a>
+                  </li>
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <h1 className="title lg:text-2xl">
+            subscribe
+          </h1>
+          <p className="text-sm mb-5">Join thousands of teachers making a difference everyday</p>
+          <div className="flex flex-row">
+            <input className="h-[50px] max-w-[272px] px-6 outline-none placeholder:text-gray-400 text-gray-600 rounded-l-lg " type="text" placeholder="Enter Email Address" />
+            <button className="h-[50px] w-[50px] flex justify-center items-center rounded-r-lg bg-slate-300 hover:bg-slate-200"><HiOutlineChevronRight /></button>
+          </div>
+        </div>
+      </div>
+    </div></footer>;
+};
+
+export default Footer;
