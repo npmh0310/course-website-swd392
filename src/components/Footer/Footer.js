442ed429eae715481e1057ecb074f7f5dbd587ac
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-03-06 11:20:19 +0700

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,9 @@ import { HiOutlineChevronRight } from "react-icons/hi";
 import './footer.css'
 const Footer = () => {
   const { company, social, support } = footer
-  return <footer className='bg-primary  section'>
+  return <footer className='bg-primary pt-16 pb-12'>
     <div className='container mx-auto'>
-      <div className="flex flex-col gap-y-8 lg:flex-row lg:gap-x-20  ">
+      <div className="flex flex-col gap-y-8 lg:flex-row lg:gap-x-20 pb-12 border-b border-black ">
         <div className="mb-5">
           <img src={Logo} className="w-28 lg:w-36" alt="" />
         </div>
@@ -59,6 +59,10 @@ const Footer = () => {
             <button className="h-[50px] w-[50px] flex justify-center items-center rounded-r-lg bg-slate-300 hover:bg-slate-200"><HiOutlineChevronRight /></button>
           </div>
         </div>
+      </div>
+      <div className="pt-14 text-center">
+        <span className="text-lg  font-bold">Course Builder | "We're here to support you every step of the way."</span>
+        <p className="text-sm mt-3 text-gray-500">© Powered by BRAINITY.</p>
       </div>
     </div></footer>;
 };
