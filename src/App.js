e5fc831d183a71ed4ded19191cb082446c8496a0
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-03-16 13:52:33 +0700

    user page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@
 import { React } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/Register Login/LoginPage';
 import PathPage from './pages/PathPage';
-import SignUpPage from './pages/SignUpPage';
+import SignUpPage from './pages/Register Login/RegisterForm';
 import BlogPage from "./pages/BlogPage";
-
+import LoginRegister from './pages/Register Login/LoginRegister';
+import LearningPage from "./pages/LearningPage"
 
 
 function App() {
   return <BrowserRouter >
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="login" element={<LoginPage />} />
+      <Route path="signin" element={<LoginPage />} />
       <Route path="signup" element={<SignUpPage />} />
       <Route path="path" element={<PathPage />} />
-      <Route path="blog" element={<BlogPage />} />
+      <Route path="blog" element={<LearningPage />} />
     </Routes>
   </BrowserRouter>
 
